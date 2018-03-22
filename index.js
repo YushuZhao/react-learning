e992e4bf715547ae4867082546a054441474eac8
yushu <yushu.zhao@roboterra.com>
2018-03-22 15:10:35 +0800

    Add Hello

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeter from './src/component/Greeter'
+
+ReactDOM.render(
+    <Greeter />, 
+    document.getElementById('root')
+);
