13dc9e0ca8f5875f921727f4b3c83a0b650e7c4d
yushu <yushu.zhao@roboterra.com>
2018-03-22 18:11:28 +0800

    carouser

--- src/component/Greeter.js
+++ src/component/Greeter.js
@@ -1,11 +1,38 @@
 import React from 'react';
-
-export default class Greeter extends React.Component {
+import { Carousel, CarouselItem, CarouselCaption } from 'react-bootstrap';
+export default class Greeter extends React.Component  {
     render(){
         return(
-            <div>
-                Hello
-            </div>
+            <Carousel style={{width:900,height:500}}>
+  <Carousel.Item>
+    <img width={900} height={500} alt="900x500" src="../../static/images/carousel.png" />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={900} height={500} alt="900x500" src="../../static/images/carousel.png" />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img width={900} height={500} alt="900x500" src="../../static/images/carousel.png" />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
         )
     }
 }
+
+
+    
+  
+  
+
+
